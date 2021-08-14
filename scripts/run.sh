e6c9727c8305bcd1ab6b59ed779fc7d83c8ab31b
marcbrandner <marc_brandner@web.de>
2021-08-14 17:18:13 +0200

    ensure cache folder is writable for container

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,6 +12,7 @@ echo -e "\n--- Run Image"
 
 JLAB_IMAGE='jupyterlab-custom:latest'
 mkdir -p /tmp/python-cache
+sudo chmod 777 -R /tmp/python-cache/
 
 if docker images | grep jupyterlab-custom.*latest &> /dev/null; [ $? -ne 0 ]; then
     echo "Docker image '$JLAB_IMAGE' not available yet. Skip attempt to run the image."
